9fde896cf946d0533713ac54dc9f49643c940267
Nguyen Phuc <phucunsse183483@fpt.edu.vn>
2024-05-02 18:29:06 +0700

    Delete _cityWeatherList

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -6,32 +6,9 @@ using WeatherServices;
 namespace WeatherAppDI.Controllers {
     public class WeatherController : Controller {
         private readonly IWeatherService _weatherService;
-        private readonly List<CityWeather> _cityWeatherList;
 
         public WeatherController(IWeatherService weatherService) {
             _weatherService = weatherService;
-            _cityWeatherList = new List<CityWeather>() {
-                new CityWeather() {
-                    CityUniqueCode = "LDN",
-                    CityName = "London",
-                    DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),
-                    TemperatureFahrenheit = 33
-                },
-
-                new CityWeather() {
-                    CityUniqueCode = "NYC",
-                    CityName = "London",
-                    DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),
-                    TemperatureFahrenheit = 60
-                },
-
-                new CityWeather() {
-                    CityUniqueCode = "PAR",
-                    CityName = "Paris",
-                    DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),
-                    TemperatureFahrenheit = 82
-                }
-            };
         }
         [Route("/")]
         public IActionResult Index() {
